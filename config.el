df8755705a5b399dc7db36efacd29cd9b9109fa4
Johan Abelson <johan@johanabelson.com>
2020-03-16 09:34:36 +0100

    Some changes to config...

--- config.el
+++ config.el
@@ -1,4 +1,4 @@
-
+(load-theme 'doom-spacegrey)
 (add-hook! 'org-mode-hook)
 
 (setq doom-font (font-spec :family "Fira Code" :size 14))
@@ -11,22 +11,22 @@
 
 ;; Enable tabs
 (setq custom-tab-width 4)
-(defun disable-tabs () (setq indent-tabs-mode nil))
+;; (defun disable-tabs () (setq indent-tabs-mode nil))
 (defun enable-tabs  ()
   (local-set-key (kbd "TAB") 'tab-to-tab-stop)
-  ;; (setq indent-tabs-mode t)
   (setq tab-width custom-tab-width)
-  (setq web-mode-indent-style 1)
-  (setq web-mode-script-padding nil)
+  (setq indent-tabs-mode t)
+  (setq web-mode-indent-style 2)
+  (setq web-mode-script-padding 4)
   )
 
 (add-hook 'web-mode-hook 'enable-tabs)
 (add-hook 'php-mode-hook 'enable-tabs)
-(add-hook 'js2-mode-hook 'enable-tabs)
-
+(add-hook 'php-mode-maybe-hook 'enable-tabs)
+(add-hook 'js2-indent-hook 'enable-tabs)
+(add-hook 'tsv-mode-hook 'enable-tabs)
 ;; -
 
-  (setq indent-tabs-mode t)
 ;; Web mode
 (setq web-mode-enable-current-element-highlight t
       web-mode-enable-current-column-highlight nil)
@@ -50,6 +50,14 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cr" 'org-refile)
 (define-key global-map "\C-cc" 'org-capture)
+;; (define-key global-map (kbd "C-M-<down>") 'scroll-down-line)
+;; (define-key global-map (kbd "C-M-<up>") 'scroll-up-line)
+(define-key global-map (kbd "<home>") 'mu4e)
+
+
+;; (map :prefix "d D"
+;;      :keymaps 'smartparens-mode-map
+;;      "S" '(sp-splice-sexp-killing-around :wk "Splice"))
 
 ;; Need to use right modifier for some characters
 (setq mac-right-option-modifier 'right)
@@ -64,8 +72,15 @@
  org-agenda-skip-deadline-if-done t
  org-agenda-todo-ignore-with-date t
  org-agenda-skip-deadline-prewarning-if-scheduled nil
- org-agenda-skip-scheduled-if-deadline-is-shown t
- 
+ org-agenda-skip-scheduled-if-deadline-is-shown nil
+ ;; org-agenda-start-day "-2d"
+ org-agenda-start-day nil
+ org-agenda-start-on-weekday nil
+ org-agenda-span 'week
+ org-deadline-warning-days 3
+ org-habit-show-habits t
+ org-habit-show-habits-only-for-today nil
+
  ;; Agenda size
  org-agenda-window-setup 'reorganize-frame
  org-agenda-window-frame-fractions '(0.75 . 1.25)
@@ -73,7 +88,8 @@
 
  org-reverse-note-order t
  org-todo-keyword-faces '(("TODO" . "YellowGreen")
-                          ("WAITING" . "Yellow")
+                          ("PROJ" . "GreenYellow")
+                          ("WAIT" . "Yellow")
                           ("DONE" . "DimGray")
                           ("CANCELLED" . "DarkRed"))
 
@@ -87,7 +103,7 @@
 
 (after! org
   (setq org-capture-templates '(("t" "Todo [inbox]" entry
-                                 (file+headline "~/Dropbox/Org/gtd/inbox.org" "Tasks")
+                                 (file+headline "~/Dropbox/Org/gtd/gtd.org" "INBOX")
                                  "* TODO %i%?")
                                 ;; Tickler item
                                 ("T" "Tickler" entry
@@ -102,19 +118,18 @@
         +org-export-directory org-directory
         org-archive-location "~/Dropbox/Org/Archive/archive.org::* From %s"
         org-default-notes-file "~/Dropbox/Org/notes/notes.org"
-        +org-capture-todo-file "~/Dropbox/Org/gtd/inbox.org"
+        +org-capture-todo-file "~/Dropbox/Org/gtd/gtd.org"
         +org-capture-notes-file "~/Dropbox/Org/notes/notes.org"
         +org-capture-journal-file "~/Dropbox/Org/journal/journal.org"
         +org-capture-projects-file "~/Projects/projects.org"
         
-        org-agenda-files '("~/Dropbox/Org/gtd/inbox.org"
-                           "~/Dropbox/Org/gtd/gtd.org"
+        org-agenda-files '("~/Dropbox/Org/gtd/gtd.org"
                            "~/Dropbox/Org/gtd/tickler.org")
 
         org-refile-targets '(("~/Dropbox/Org/gtd/gtd.org" :maxlevel . 2)
                              ("~/Dropbox/Org/gtd/someday.org" :maxlevel . 1)
                              ("~/Dropbox/Org/gtd/tickler.org" :maxlevel . 1))
-                
+
         ;; ;; Set Org tags
 
         org-tag-alist '(("@computer" . ?c) ("@offline" . ?o)
@@ -129,40 +144,97 @@
                         )
 
         ;; Keywords
-        org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)"))
+        org-todo-keywords '((sequence "TODO(t)" "PROJ(+)" "WAIT(w)" "|" "DONE(d)"))
         )
 
+  (defun air-org-skip-subtree-if-priority (priority)
+    "Skip an agenda subtree if it has a priority of PRIORITY.
+
+PRIORITY may be one of the characters ?A, ?B, or ?C."
+    (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+          (pri-value (* 1000 (- org-priority-lowest priority)))
+          (pri-current (org-get-priority (thing-at-point 'line t))))
+      (if (= pri-value pri-current)
+          subtree-end
+        nil)))
+
+(defun air-org-skip-subtree-if-habit ()
+  "Skip an agenda entry if it has a STYLE property equal to \"habit\"."
+  (let ((subtree-end (save-excursion (org-end-of-subtree t))))
+    (if (string= (org-entry-get nil "STYLE") "habit")
+        subtree-end
+      nil)))
+
   ;; (key desc type match settings files)
+  ;; (setq org-agenda-custom-commands
+  ;;       '(
+  ;;         ("c" "Simple agenda view"
+  ;;          ((tags "PRIORITY=\"A\""
+  ;;                 ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+  ;;                  (org-agenda-overriding-header "High-priority unfinished tasks:")))
+  ;;           (agenda "")
+  ;;           (alltodo ""
+  ;;                    ((org-agenda-skip-function
+  ;;                      '(or (air-org-skip-subtree-if-priority ?A)
+  ;;                           (org-agenda-skip-if nil '(scheduled deadline))))))))
+  ;;         ("t" "TODAY"
+  ;;          ((agenda)
+  ;;           (tags-todo "@work"))
+  ;;          ((org-agenda-overriding-header "Today's Tasks")
+  ;;           (org-agenda-files '("~/Dropbox/Org/gtd/"))
+  ;;           (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("WAITING")))
+  ;;           (org-agenda-span 1))
+  ;;          )
+  ;;         ("w" "Work-mode" ((agenda) (tags-todo "+PRIORITY=\"A\"+@work"))
+  ;;          ((org-agenda-files '("~/Dropbox/Org/gtd/gtd.org" "~/Dropbox/Org/notes/" "~/Dropbox/Org/gtd/projects.org"))
+  ;;           (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'todo '("WAITING")))
+  ;;           (org-agenda-sorting-strategy '(priority-down effort-down)))
+  ;;          )
+  ;;         ("W" "Planning Work" tags-todo "@work"
+  ;;          ((org-agenda-files '("~/Dropbox/Org/gtd/gtd.org" "~/Dropbox/Org/notes/" "~/Dropbox/Org/gtd/projects.org"))
+  ;;           (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled))
+  ;;           (org-agenda-sorting-strategy '(priority-down effort-down)))
+  ;;          )
+  ;;         ("i" "InPress-mode" tags-todo "+CATEGORY=\"InPress\"|@inpress"
+  ;;          ((org-agenda-files '("~/Dropbox/Org/gtd/gtd.org" "~/Dropbox/Org/notes/"))
+  ;;           (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("WAITING" "REVIEW" "IDEA" "JÖRGEN")))
+  ;;           (org-agenda-sorting-strategy '(priority-down effort-down)))
+  ;;          )
+  ;;         ("h" "Home-mode" tags-todo "@home"
+  ;;          ((org-agenda-files '("~/Dropbox/Org/gtd/gtd.org"))
+  ;;           (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'todo '("WAITING")))
+  ;;           (org-agenda-sorting-strategy '(priority-down effort-down)))
+  ;;          )
+  ;;         ("5" "5 mins" tags-todo "+5min-@inpress"
+  ;;          ((org-agenda-files '("~/Dropbox/Org/gtd/gtd.org"))
+  ;;           (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'todo '("WAITING")))
+  ;;           (org-agenda-sorting-strategy '(priority-down effort-down)))
+  ;;          )
+  ;;         ("l" tags "+@leaving")
+  ;;         ("k" tags "+@kate")
+  ;;         ))
+
   (setq org-agenda-custom-commands
-        '(("w" "Work-mode" tags-todo "+PRIORITY=\"A\"+@work"
-           ((org-agenda-files '("~/Dropbox/Org/gtd/gtd.org" "~/Dropbox/Org/notes/" "~/Dropbox/Org/gtd/projects.org"))
-            (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'todo '("WAITING")))
-            (org-agenda-sorting-strategy '(priority-down effort-down)))
-           )
-          ("W" "Planning Work" tags-todo "@work"
-           ((org-agenda-files '("~/Dropbox/Org/gtd/gtd.org" "~/Dropbox/Org/notes/" "~/Dropbox/Org/gtd/projects.org"))
-            (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled))
-            (org-agenda-sorting-strategy '(priority-down effort-down)))
-           )
-          ("i" "InPress-mode" tags-todo "+CATEGORY=\"InPress\"|@inpress"
-           ((org-agenda-files '("~/Dropbox/Org/gtd/gtd.org" "~/Dropbox/Org/notes/"))
-            (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("WAITING" "REVIEW" "IDEA" "JÖRGEN")))
-            (org-agenda-sorting-strategy '(priority-down effort-down)))
-           )
-          ("h" "Home-mode" tags-todo "@home"
-           ((org-agenda-files '("~/Dropbox/Org/gtd/gtd.org"))
-            (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'todo '("WAITING")))
-            (org-agenda-sorting-strategy '(priority-down effort-down)))
-           )
-          ("5" "5 mins" tags-todo "+5min-@inpress"
-           ((org-agenda-files '("~/Dropbox/Org/gtd/gtd.org"))
-            (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'todo '("WAITING")))
-            (org-agenda-sorting-strategy '(priority-down effort-down)))
-           )
-          ("l" tags "+@leaving")
-          ("k" tags "+@kate")
-))
-        ;; (setq org-agenda-include-diary t)
+        '(("d" "Daily agenda and all TODOs"
+           ((tags "PRIORITY=\"A\""
+                  ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                   (org-agenda-overriding-header "TODAY:")))
+            (agenda)
+            (alltodo ""
+                     ((org-agenda-skip-function '(or (air-org-skip-subtree-if-habit)
+                                                     (air-org-skip-subtree-if-priority ?A)
+                                                     (org-agenda-skip-if nil '(scheduled deadline))))
+                      (org-agenda-overriding-header "NEXT:")))
+            (tags-todo "+CATEGORY=\"InPress\"|@inpress"
+                       ((org-agenda-files '("~/Dropbox/Org/gtd/gtd.org" "~/Dropbox/Org/notes/"))
+                        (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("WAIT" "REVIEW" "IDEA" "JÖRGEN")))
+                        (org-agenda-sorting-strategy '(priority-down effort-down))
+                        (org-agenda-overriding-header "@InPress:")))
+            )
+           ;; ((org-agenda-compact-blocks t))
+           )))
+
+  (setq org-agenda-include-diary nil)
 
   (set-face-attribute 'org-link nil
                       :weight 'normal
@@ -225,6 +297,61 @@
  magit-repository-directories '(("~/Projects" . 2))
  )
 
+;; Mail - mu4e
+(require 'mu4e)
+;;location of my maildir
+(setq mu4e-maildir (expand-file-name "~/mbsync"))
+
+(setq mu4e-enable-notifications t)
+(setq mu4e-attachment-dir  "~/Downloads")
+(setq mu4e-enable-mode-line t)
+
+(setq mu4e-change-filenames-when-moving t)
+(setq mail-user-agent 'mu4e-user-agent)
+(setq mu4e-drafts-folder "/[Gmail].Drafts")
+(setq mu4e-sent-folder   "/[Gmail].Sent Mail")
+(setq mu4e-trash-folder  "/[Gmail].Trash")
+(setq mu4e-refile-folder "/[Gmail].All Mail")
+(setq mu4e-sent-messages-behavior 'delete)
+(setq mu4e-headers-skip-duplicates t)
+(setq mu4e-use-fancy-chars t)
+(setq mu4e-alert-interesting-mail-query "flag:unread AND maildir:~/INBOX")
+(setq mu4e-display-update-status-in-modeline t)
+(setq mu4e-hide-index-messages t)
+(setq mu4e-headers-include-related nil)
+(setq mu4e-update-interval 300)
+(setq mu4e-html2text-command "w3m -dump -T text/html -o display_link_number=true")
+(setq mu4e-maildir-shortcuts
+      '( ("/INBOX"               . ?i)
+         ("/[Gmail].Sent Mail"   . ?s)
+         ("/[Gmail].Trash"       . ?t)
+         ("/[Gmail].All Mail"    . ?a)))
+;; (setq mu4e-get-mail-command "offlineimap")
+(setq mu4e-get-mail-command "mbsync -c ~/.mbsyncrc gmail")
+(setq user-mail-address "johan@johanabelson.com")
+(setq mu4e-user-mail-address-list '("johan@johanabelson.com"))
+(setq mu4e-compose-dont-reply-to-self t)
+(setq mu4e-compose-format-flowed t)
+(setq mu4e-compose-complete-only-personal t)
+(setq user-full-name "Johan Abelson")
+(setq mu4e-compose-mode-hook
+      (lambda ()
+        (use-hard-newlines -1)
+        (turn-off-auto-fill)
+        (visual-line-mode)))
+(require 'smtpmail)
+(setq message-send-mail-function 'smtpmail-send-it
+      starttls-use-gnutls t
+      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials '(("smtp.gmail.com" 587 "johan@johanabelson.com" "xitneV-gybcy9-tycxiz"))
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587)
+(setq message-kill-buffer-on-exit t)
+;;set up queue for offline email
+;;use mu mkdir  ~/mbsync/queue to set up first
+(setq smtpmail-queue-mail nil  ;; start in normal mode
+      smtpmail-queue-dir   "~/mbsync/queue/cur")
 ;; Language
 (add-hook!
  js2-mode 'prettier-js-mode
--- init.el
+++ init.el
@@ -1,22 +1,28 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
-;; Copy this file to ~/.doom.d/init.el or ~/.config/doom/init.el ('doom install'
-;; will do this for you). The `doom!' block below controls what modules are
-;; enabled and in what order they will be loaded. Remember to run 'doom refresh'
-;; after modifying it.
+;; This file controls what Doom modules are enabled and what order they load in.
+;; Remember to run 'doom sync' after modifying it!
+
+;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
+;;      documentation. There you'll find information about all of Doom's modules
+;;      and what flags they support.
+
+;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
+;;      'C-c g k' for non-vim users) to view its documentation. This works on
+;;      flags as well (those symbols that start with a plus).
 ;;
-;; More information about these modules (and what flags they support) can be
-;; found in modules/README.org.
+;;      Alternatively, press 'gd' (or 'C-c g d') on a module to browse its
+;;      directory (for easy access to its source code).
 
 (doom! :input
        ;;chinese
        ;;japanese
 
        :completion
-       (company +childframe)          ; the ultimate code completion backend
-       ;;helm              ; the *other* search engine for love and life
+       (company +tng)          ; the ultimate code completion backend
+       helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       ;; ivy               ; a search engine for love and life
 
        :ui
        deft              ; notational velocity for Emacs
@@ -24,7 +30,7 @@
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;fill-column       ; a `fill-column' indicator
-       ;; hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
        modeline          ; snazzy, Atom-inspired modeline, plus API
@@ -36,12 +42,13 @@
         +defaults)       ; default popup rules
        ;;pretty-code       ; replace bits of code with pretty symbols
        ;;tabs              ; an tab bar for Emacs
-       ;; treemacs          ; a project drawer, like neotree but cooler
+       ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
+       zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
@@ -49,25 +56,30 @@
        fold              ; (nigh) universal code folding
        ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
-       ;;lispy             ; vim for lisp, for people who dont like vim
+       ;;lispy             ; vim for lisp, for people who don't like vim
        multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
-       ;; rotate-text       ; cycle region at point between text candidates
+       rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
-       ;; word-wrap         ; soft wrapping with language-aware indent
+       ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
        dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
-       ;; ibuffer           ; interactive buffer management
-       ;; vc                ; version-control and Emacs, sitting in a tree
+       ibuffer           ; interactive buffer management
+       vc                ; version-control and Emacs, sitting in a tree
 
        :term
        eshell            ; a consistent, cross-platform shell (WIP)
        ;; shell             ; a terminal REPL for Emacs
        ;; term              ; terminals in Emacs
        ;;vterm             ; another terminals in Emacs
+
+       :checkers
+       syntax              ; tasing you for every semicolon you forget
+       ;;spell             ; tasing you for misspelling mispelling
+       ;;grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
@@ -77,8 +89,6 @@
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       flycheck          ; tasing you for every semicolon you forget
-       ;;flyspell          ; tasing you for misspelling mispelling
        ;;gist              ; interacting with github gists
        (lookup           ; helps you navigate your code and documentation
         +docsets)        ; ...or in Dash docsets locally
@@ -93,7 +103,6 @@
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
-       ;;wakatime
 
        :lang
        ;;agda              ; types of types of types of types...
@@ -112,8 +121,9 @@
        ;;ess               ; emacs speaks statistics
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp           ; ML stands for Microsoft's Language
+       ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;go                ; the hipster dialect
-       ;;(haskell +intero) ; a language that's lazier than I am
+       ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
@@ -122,9 +132,10 @@
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean
+       ;;factor
        ;;ledger            ; an accounting system in Emacs
        ;;lua               ; one-based indices? one-based indices
-       ;; markdown          ; writing docs for people to ignore
+       markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
@@ -132,9 +143,9 @@
         +dragndrop       ; drag & drop files/images into org buffers
         +hugo            ; use Emacs for hugo blogging
         +journal
-        ;; +ipython         ; ipython/jupyter support for babel
+        ;; +jupyter         ; ipython/jupyter support for babel
         ;; +pandoc          ; export-with-pandoc support
-        ;+pomodoro        ; be fruitful with the tomato technique
+        ;;+pomodoro        ; be fruitful with the tomato technique
         +present)        ; using org-mode for presentations
        ;;perl              ; write code no one else can comprehend
        (php +lsp)               ; perl's insecure younger brother
@@ -143,40 +154,29 @@
        ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
-       ;; rest              ; Emacs as a REST client
+       ;;rest              ; Emacs as a REST client
+       ;;rst               ; ReST in peace
        ;;ruby              ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
-       ;; (sh +lsp)                ; she sells {ba,z,fi}sh shells on the C xor
+       sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        (web +lsp)          ; the tubes
 
        :email
-       ;; (mu4e +gmail)       ; WIP
-       ;;notmuch             ; WIP
-       ;;(wanderlust +gmail) ; WIP
+       ;;(mu4e +gmail)
+       ;;notmuch
+       ;;(wanderlust +gmail)
 
-       ;; Applications are complex and opinionated modules that transform Emacs
-       ;; toward a specific purpose. They may have additional dependencies and
-       ;; should be loaded late.
        :app
        ;;calendar
-       ;; irc               ; how neckbeards socialize
+       ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
-       ;;(write            ; emacs for writers (fiction, notes, papers, etc.)
-       ;; +wordnut         ; wordnet (wn) search
-       ;; +langtool)       ; a proofreader (grammar/style check) for Emacs
 
        :config
-       ;; For literate config users. This will tangle+compile a config.org
-       ;; literate config in your `doom-private-dir' whenever it changes.
        ;;literate
-
-       ;; The default module sets reasonable defaults for Emacs. It also
-       ;; provides a Spacemacs-inspired keybinding scheme and a smartparens
-       ;; config. Use it as a reference for your own modules.
        (default +bindings +smartparens))
